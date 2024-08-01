8217729075c2d4dd57e66561c277aa61bb871fc0
othoncampos <othonstuart@gmail.com>
2024-08-01 10:37:10 -0300

    Arquivos iniciais
    Arquivos iniciais do projeto

---
+++ src/main/java/br/ifba/demo/frontend/DemoFrontendApplication.java
@@ -0,0 +1,13 @@
+package br.ifba.demo.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFrontendApplication.class, args);
+	}
+
+}
